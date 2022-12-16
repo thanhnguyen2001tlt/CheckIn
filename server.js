0ee4f950cf57ef8e0c4fe74bda89f8dda4e55c7f
thanhnguyen2001tlt <ntthanha20158@cusc.ctu.edu.vn>
2022-12-16 17:30:32 +0700

    upload

--- server.js
+++ server.js
@@ -7,26 +7,31 @@ mongoose.set('strictQuery', false);
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, }).then(() => console.log("MongoDB Connected....")).catch((err) => console.log(err));;
 const wifi = require('node-wifi');
 const address = require('address');
-const { mac } = require("address");
 wifi.init({
   iface: null,
 });
-var check = 1;
-var ssiddb = "MADIAD - LIVETRADE";
-var macdb = "60:6d:c7:ef:cc:a7";
-var wfh = 1;
-wifi.getCurrentConnections((error, currentConnections) => {
-  var ssid = currentConnections[0].ssid;
-  if (ssiddb === ssid) {
-    address.mac(function (err, mac) {
-        if(mac === macdb){
+
+//get mac
+function getmac(){
+  var mac;
+  address.mac(function (err, m) {
+      mac =m;
+  });
+  return mac;
+}
+//check
+function checkin(ssiddb, macdb, wfh) {
+  var check = 1;
+  wifi.getCurrentConnections((error, currentConnections) => {
+    var ssid = currentConnections[0].ssid;
+    if (ssiddb === ssid && macdb == getmac()) {
           check = 0;
         }
-    });
-  } else if(wfh == 0) {
-    check = 0;
-  }
-  console.log(check);
-});
+      });
+  return check;
+}
+
+console.log(checkin("MADIAD - LIVETRADE ", "60:6d:c7:ef:cc:a7", 1));
+console.log(getmac());
 const PORT = process.env.PORT || 8080;
 app.listen(PORT) 
