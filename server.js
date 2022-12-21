2c8c65cb54354916dad97c0b10b31bc4105df9e2
thanhnguyen2001tlt <ntthanha20158@cusc.ctu.edu.vn>
2022-12-21 15:36:37 +0700

    s

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ app.use("/api/check", require("./router/checkin"));
 app.use("/api/check", require("./router/checkout"));
 app.use("/api/check", require("./router/getmac"));
 app.use("/api/check", require("./router/getwifi"));
-
+var detectSSid = require('detect-ssid');
+detectSSid(function(error, ssidname) {
+    console.log(ssidname);
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT) 
