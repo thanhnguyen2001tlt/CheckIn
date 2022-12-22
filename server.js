542e61a20b25ef3a8f59339a71a380333696bc5c
thanhnguyen2001tlt <ntthanha20158@cusc.ctu.edu.vn>
2022-12-22 16:43:14 +0700

    hello

--- server.js
+++ server.js
@@ -9,8 +9,23 @@ app.use("/api/check", require("./router/checkin"));
 app.use("/api/check", require("./router/checkout"));
 app.use("/api/check", require("./router/getmac"));
 app.use("/api/check", require("./router/getwifi"));
- 
+// var dnsSync = require('dns-sync');
+// console.log(dnsSync.resolve('madiad.ddns.net'));  
+
+// const dns = require('dns');
+
+// const MAC_address = '00:11:22:33:44:55';
+
+// dns.resolve4(MAC_address, (err, addresses) => {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log(addresses);
+//   }
+// });
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT) 
+
 
