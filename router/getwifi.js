f1814c39300b5d14bd441e992cbd9429138b59c1
thanhnguyen2001tlt <ntthanha20158@cusc.ctu.edu.vn>
2022-12-27 08:51:42 +0700

    hello

--- router/getwifi.js
+++ router/getwifi.js
@@ -13,21 +13,38 @@ getWifissid.post("/getwifi", async (req, res) => {
       response.on('data', (chunk) => {
         data += chunk;
       });
-      const ips = process.env.IP;
-      response.on('end', () => {
-       const ipar =JSON.parse(data).ip;
-        if (JSON.parse(data).ip !== dns) {
-            return res.status(400).json({
-                message: "WFH",
-                status: false,ips,
-            })
-        } else {
-            return res.status(200).json({
-                message: "Cty",
-                status: true,ips,
-            })
-        }
+      const http = require('http');
+
+      const server = http.createServer((request, response) => {
+        const ips = request.connection.remoteAddress;
+        if (!ip) {
+                  return res.status(400).json({
+                      message: "WFH",
+                      status: false,ips,
+                  })
+              } else {
+                  return res.status(200).json({
+                      message: "Cty",
+                      status: true,ips,
+                  })
+              }
+        response.end();
       });
-    });
+      
+    //   response.on('end', () => {
+    //    const ipar =JSON.parse(data).ip;
+    //     if (JSON.parse(data).ip !== dns) {
+    //         return res.status(400).json({
+    //             message: "WFH",
+    //             status: false,ipar,
+    //         })
+    //     } else {
+    //         return res.status(200).json({
+    //             message: "Cty",
+    //             status: true,ipar,
+    //         })
+    //     }
+    //   });
+     });
 });
 module.exports = getWifissid;
